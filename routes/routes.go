12f5042101d191ccd506e5171cac214ca0fb3854
fabricio.vertamatti <fabricio.vertamatti@telefonica.com>
2024-11-28 14:37:29 -0300

    Middleware para set content type json

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/favert/api-go-rest/controllers"
+	"github.com/favert/api-go-rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
