040250c5eeddfcee56bb9e07fa1ca3d4b97ada52
fabricio.vertamatti <fabricio.vertamatti@telefonica.com>
2024-12-04 09:04:06 -0300

    Added CORS handler.

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/favert/api-go-rest/controllers"
 	"github.com/favert/api-go-rest/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -18,5 +19,5 @@ func HandleRequest() {
 	r.HandleFunc("/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/personalidades/{id}", controllers.EditaUmaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
